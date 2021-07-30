c2060258eebe3fc4ccd2c0d671d18ab7993731af
dosgo <dosgo@qq.com>
2021-07-30 14:23:45 +0800

    ..

--- comm/netstat/win.go
+++ comm/netstat/win.go
@@ -16,10 +16,6 @@ func PortGetPid(lSocks string) (int,error) {
 	if err != nil {
 		return 0, err
 	}
-	snp, err := netstat.CreateToolhelp32Snapshot(netstat.Th32csSnapProcess, 0)
-	if err != nil {
-		return 0, err
-	}
 	s := tbl.Rows()
 	for i := range s {
 		ent:= netstat.SockTabEntry{
@@ -28,24 +24,15 @@ func PortGetPid(lSocks string) (int,error) {
 			State:      s[i].SockState(),
 		}
 		if ent.State == netstat.Listen && ent.LocalAddr.Port==uint16(lPort) {
-			process:=s[i].Process(snp);
-			if process!=nil {
-				return process.Pid, nil;
-			}
+			return int(s[i].WinPid), nil;
 		}
 	}
-	snp.Close()
 	return 0, nil;
 }
 
 
 func IsSocksServerAddr(pid int,addr string)bool{
 	tbl, err := netstat.GetTCPTable2(true)
-	if err != nil {
-		return  false;
-	}
-	snp, err := netstat.CreateToolhelp32Snapshot(netstat.Th32csSnapProcess, 0)
-	defer 	snp.Close();
 	if err != nil {
 		return  false;
 	}
@@ -58,8 +45,7 @@ func IsSocksServerAddr(pid int,addr string)bool{
 		}
 		if ent.State == netstat.Established  || ent.State == netstat.FinWait1 ||ent.State == netstat.FinWait2 || ent.State == netstat.SynSent {
 			if strings.Index(ent.RemoteAddr.String(),addr)!=-1 {
-				process:=s[i].Process(snp);
-				if process!=nil  && process.Pid==pid  {
+				if  int(s[i].WinPid)==pid  {
 					return true;
 				}
 			}
