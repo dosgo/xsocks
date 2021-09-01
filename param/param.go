ddc7c36cfbbd19101333b9d0557b5b4d375361b4
DosGo <dosgo@qq.com>
2021-09-01 21:20:33 +0800

    update version

--- param/param.go
+++ param/param.go
@@ -2,15 +2,15 @@ package param
 
 import "time"
 
-var version ="1.7.7-(20210801)"
+var version = "1.7.8-(20210901)"
 
-var Args *ArgsParam;
+var Args *ArgsParam
 
-func init(){
-	Args=&ArgsParam{};
-	Args.ConnectTime=time.Second*10;
-	Args.Version=version;
-	Args.SafeDns="114.114.114.114";
+func init() {
+	Args = &ArgsParam{}
+	Args.ConnectTime = time.Second * 10
+	Args.Version = version
+	Args.SafeDns = "114.114.114.114"
 }
 
 type ArgsParam struct {
@@ -19,39 +19,38 @@ type ArgsParam struct {
 	ServerParam
 }
 
-
 type CommParam struct {
-	Version string;
-	Mtu int;
-	Password string
-	ConnectTime  time.Duration `json:"-"` //=10*time.Second;
+	Version     string
+	Mtu         int
+	Password    string
+	ConnectTime time.Duration `json:"-"` //=10*time.Second;
 	UdpGatePort string
 }
 
-type ClientParam struct{
-	 Sock5Addr string
-	 ServerAddr string
-	 CaFile string;
-	 SkipVerify bool;
-	 TunType int;
-	 UnixSockTun string;
-	 DnsPort string;
-	 UdpProxy int;
-	 MuxNum int;
-	 LocalDns int;  //use local dns
-	 SmartDns int;  //use Smart dns
-	 TunSmartProxy bool;
-	 Sock5UdpPort string
+type ClientParam struct {
+	Sock5Addr     string
+	ServerAddr    string
+	CaFile        string
+	SkipVerify    bool
+	TunType       int
+	UnixSockTun   string
+	DnsPort       string
+	UdpProxy      int
+	MuxNum        int
+	LocalDns      int //use local dns
+	SmartDns      int //use Smart dns
+	TunSmartProxy bool
+	Sock5UdpPort  string
 }
 
 type ServerParam struct {
-	 Sock5Port string
-	 QuicPort string
-	 WebPort string
-	 SudpPort string;
-	 KcpPort string;
-	 CertFile string;
-	 KeyFile string;
-	 LocalTunSock string;
-	 SafeDns string;
+	Sock5Port    string
+	QuicPort     string
+	WebPort      string
+	SudpPort     string
+	KcpPort      string
+	CertFile     string
+	KeyFile      string
+	LocalTunSock string
+	SafeDns      string
 }
