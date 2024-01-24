0f4154f6dc2d6becaf5212bc8c7bafacfaedc9de
dosgo <dosgo@qq.com>
2024-01-24 22:53:22 +0800

    Update nettool.go

--- comm/nettool.go
+++ comm/nettool.go
@@ -1,59 +1,10 @@
 package comm
 
 import (
-	"math/rand"
 	"net"
-	"strconv"
-	"strings"
 	"time"
 )
 
-/*根据子网掩码返回一个IP段*/
-func GetCidrRandIpByNet(tunAddr string, tunMask string) string {
-	masks := net.ParseIP(tunMask).To4()
-	maskAddr := net.IPNet{IP: net.ParseIP(tunAddr), Mask: net.IPv4Mask(masks[0], masks[1], masks[2], masks[3])}
-	return GetCidrRandIp(maskAddr.String())
-}
-
-func GetCidrRandIp(cidr string) string {
-	ip := strings.Split(cidr, "/")[0]
-	ipSegs := strings.Split(ip, ".")
-	maskLen, _ := strconv.Atoi(strings.Split(cidr, "/")[1])
-	seg3MinIp, seg3MaxIp := getIpSeg3Range(ipSegs, maskLen)
-	seg4MinIp, seg4MaxIp := getIpSeg4Range(ipSegs, maskLen)
-	ipPrefix := ipSegs[0] + "." + ipSegs[1] + "."
-	return ipPrefix + strconv.Itoa(IntnRange(seg3MinIp, seg3MaxIp)) + "." + strconv.Itoa(IntnRange(seg4MinIp, seg4MaxIp))
-}
-
-func IntnRange(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
-// 得到第三段IP的区间（第一片段.第二片段.第三片段.第四片段）
-func getIpSeg3Range(ipSegs []string, maskLen int) (int, int) {
-	if maskLen > 24 {
-		segIp, _ := strconv.Atoi(ipSegs[2])
-		return segIp, segIp
-	}
-	ipSeg, _ := strconv.Atoi(ipSegs[2])
-	return getIpSegRange(uint8(ipSeg), uint8(24-maskLen))
-}
-
-// 得到第四段IP的区间（第一片段.第二片段.第三片段.第四片段）
-func getIpSeg4Range(ipSegs []string, maskLen int) (int, int) {
-	ipSeg, _ := strconv.Atoi(ipSegs[3])
-	segMinIp, segMaxIp := getIpSegRange(uint8(ipSeg), uint8(32-maskLen))
-	return segMinIp + 1, segMaxIp
-}
-
-// 根据用户输入的基础IP地址和CIDR掩码计算一个IP片段的区间
-func getIpSegRange(userSegIp, offset uint8) (int, int) {
-	var ipSegMax uint8 = 255
-	netSegIp := ipSegMax << offset
-	segMinIp := netSegIp & userSegIp
-	segMaxIp := userSegIp&(255<<offset) | ^(255 << offset)
-	return int(segMinIp), int(segMaxIp)
-}
 
 func CheckTcp(host string, port string) bool {
 	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), time.Second*1)
@@ -66,28 +17,3 @@ func CheckTcp(host string, port string) bool {
 	return false
 }
 
-func GetLocalIpV4() string {
-	inters, err := net.Interfaces()
-	if err != nil {
-		panic(err)
-	}
-	for _, inter := range inters {
-		// 判断网卡是否开启，过滤本地环回接口
-		if inter.Flags&net.FlagUp != 0 && !strings.HasPrefix(inter.Name, "lo") {
-			// 获取网卡下所有的地址
-			addrs, err := inter.Addrs()
-			if err != nil {
-				continue
-			}
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					//判断是否存在IPV4 IP 如果没有过滤
-					if ipnet.IP.To4() != nil {
-						return ipnet.IP.String()
-					}
-				}
-			}
-		}
-	}
-	return ""
-}
