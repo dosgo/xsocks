f673c998829ca47c1ddea0711214ed960066eb22
dosgo <dosgo@qq.com>
2026-02-05 20:55:01 +0800

    update

--- client/client.go
+++ client/client.go
@@ -85,7 +85,7 @@ func (c *Client) Start() error {
 		if param.Args.UdpProxy == 1 {
 			udp = true
 		}
-		c.socksTap = socksTap.NewSocksTap(11809, param.Args.Sock5Addr, 1, udp)
+		c.socksTap = socksTap.NewSocksTap(param.Args.Sock5Addr, 1, udp)
 		//urlInfo, _ := url.Parse(param.Args.ServerAddr)
 
 		c.socksTap.Start()
@@ -102,7 +102,7 @@ func (c *Client) Start() error {
 		if param.Args.UdpProxy == 1 {
 			udp = true
 		}
-		c.socksTap = socksTap.NewSocksTap(11809, param.Args.ServerAddr, 1, udp)
+		c.socksTap = socksTap.NewSocksTap(param.Args.ServerAddr, 1, udp)
 		c.socksTap.Start()
 	}
 
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.24.1
 
 require (
 	github.com/dosgo/go-tun2socks v1.2.7
-	github.com/dosgo/goSocksTap v0.0.0-20260202081117-e4a2e970ddb4
 	github.com/hashicorp/yamux v0.1.1
 	github.com/miekg/dns v1.1.59
 	github.com/quic-go/quic-go v0.59.0
@@ -13,6 +12,8 @@ require (
 	golang.org/x/sys v0.35.0
 	golang.org/x/time v0.7.0
 )
+
+require github.com/dosgo/goSocksTap v0.0.0-20260205124528-6c94ba79580b
 
 require (
 	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5 // indirect
